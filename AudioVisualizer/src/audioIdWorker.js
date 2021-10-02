78a1d7793a5bf2ae767d0b4fa2dbe8fb4b6badee
DrAwesome4333 <Dr.Awesome4333@gmail.com>
2021-10-01 18:36:30 -0600

    Audio titles now are extracted from ID3 tags

--- AudioVisualizer/src/audioIdWorker.js
+++ AudioVisualizer/src/audioIdWorker.js
@@ -19,19 +19,24 @@ onmessage = function(e){
 
         let headerSize = getTagSize(viewOfData);
         // We are currently decoding for id3v2.3.0 see https://id3.org/d3v2.3.0
-        title = version + " ";
-        if(version == 3){
+        //title = version + " ";
+        if(version == 2 || version == 3){
             // TODO adjust for extended header size (though my sample of MP3's contains no extended headers)
             let frameStart = 10;
-            var info = getAllFrameHeaders(viewOfData, frameStart, frameStart + headerSize, usesUnSync);
+            var info = getAllFrameHeaders(viewOfData, frameStart, frameStart + headerSize, version, usesUnSync);
             if(info["TIT2"]){
-                title = "Retrieved Title: " + info["TIT2"].data;
+                title = info["TIT2"].data;
+            }else if(info["TT2"]){
+                title = info["TT2"].data;
             }else if(info["TOFN"]){
-                title = "Retrieved Title(og file name): " + info["TOFN"].data;
+                title = info["TOFN"].data;
+            }else if(info["TOF"]){
+                title = info["TOF"].data;
             }
-            postMessage(JSON.stringify(info))
+            //postMessage(JSON.stringify(info))
+            
+            //postMessage(bytesToHexString(viewOfData, 0, headerSize + frameStart));
         }
-        
     }
     
     postMessage([reqId, title, imgSrc, artist]);
@@ -59,7 +64,7 @@ function getTagSize(data){
     // This data starts with byte 6 and goes for 4 bytes, each only containing 7 bits of the full size
     var size = 0;
     for(var i = 6; i < 10; i++){
-        size << 7;
+        size = size << 7;
         size += data[i];
     }
     return size;
@@ -77,13 +82,22 @@ function getFrameID(data, frameStart){
 /**
  * @param {Uint8Array} data
  * @param {number} frameStart
+ * @param {number} frameVersion
  * @param {boolean} usesUnSync
  */
-function getFrameSize(data, frameStart, usesUnSync){
+function getFrameSize(data, frameStart, frameVersion, usesUnSync){
     var size = 0;
-    for(var i = frameStart + 4; i < frameStart + 8; i++){
-        size << 8;
-        size += data[i];
+    if(frameVersion == 3){
+        
+        for(var i = frameStart + 4; i < frameStart + 8; i++){
+            size = size << 8;
+            size += data[i];
+        }
+    }else if (frameVersion == 2){
+        for(var i = frameStart + 3; i < frameStart + 6; i++){
+            size = size << 8;
+            size += data[i];
+        }
     }
     return size;
 }
@@ -94,12 +108,52 @@ function getFrameSize(data, frameStart, usesUnSync){
  * @param {number} length
  * @param {boolean} usesUnSync
  */
-function getDataAsString(data, start, length, usesUnSync){
+function decodeTextFrameData(data, start, length, usesUnSync){
     var result = "";
-    for(var i = 0; i < length; i++){
-        result += String.fromCharCode(data[i + start]);
+    var _16Bit = 0x01 == data[start];
+    if(!_16Bit){
+        for(var i = 1; i < length; i++){
+            if(data[i + start] == 0x00){
+                // Break on Null character
+                break;
+            }
+            result += String.fromCharCode(data[i + start]);
+        }
+    }else{
+        var byteOrder = (data[start + 2] << 8) + data[start + 1]
+        if(byteOrder == 0xFEFF){
+            for(var i = 3; i + 1 < length; i += 2){
+                if ((data[start + 1 + i] << 8) + data[start + i] == 0x0000){
+                    // Break on Null character
+                    break;
+                }
+                result += String.fromCharCode((data[start + 1 + i] << 8) + data[start + i]);
+            }
+        }else if (byteOrder == 0xFFFE){
+            for(var i = 3; i + 1 < length; i += 2){
+                if ((data[start + 1 + i] << 8) + data[start + i] == 0x0000){
+                    // Break on Null character
+                    break;
+                }
+                result += (data[start + i] << 8) + data[start + i + 1];
+            }
+        }
     }
     return result;
+}
+
+function getByteAsHex(value) {
+    var str = "";
+
+    var num = value.toString(16);
+
+    while(num.length < 2){
+        num = "0" + num;
+    }
+
+    str += num;
+    
+    return str;
 }
 
 /**
@@ -116,22 +170,65 @@ function getDataBytesAsString(data, start, length){
 }
 
 
-
+const DESIRED_FRAMES = [
+    "TAL","TCM","TOA","TOF","TOL","TP1","PIC","TT2","TT3","TOF",
+    "APIC","TCOM","TIT2","TIT3","TOAL","TOFN","TOLY","TOPE","TOFN"]
 /**
  * @param {Uint8Array} data
  * @param {number} frameStart
  * @param {number} headerEnd
  * @param {boolean} usesUnSync
  */
-function getAllFrameHeaders(data, frameStart, headerEnd, usesUnSync){
+function getAllFrameHeaders(data, frameStart, headerEnd, tagVersion, usesUnSync){
     var offset = frameStart;
     var headers = {};
     while(offset < data.length && offset < headerEnd){
         var frameID = getFrameID(data, offset);
-        var frameSize = getFrameSize(data, offset, usesUnSync);
-        headers[frameID] = {start:offset, size:frameSize, data:getDataAsString(data, offset + 10, frameSize, usesUnSync)};
-        offset += frameSize + 10;
+        var frameVersion = detectFrameVersion(frameID);
+        if(frameVersion == 2){
+            frameID = frameID.substring(0, 3);
+        }
+        var frameTagSize = 10;
+
+        if(tagVersion == 2){
+            frameTagSize = 6;
+        }
+        var frameSize = getFrameSize(data, offset, tagVersion, usesUnSync);
+        // Only Decode Text frames as text
+        if (DESIRED_FRAMES.includes(frameID) && frameID[0] == "T"){
+            headers[frameID] = {start:offset, size:frameSize, data:decodeTextFrameData(data, offset + frameTagSize, frameSize, usesUnSync)};
+        }
+        offset += frameSize + frameTagSize;
     }
     return headers;
 
+}
+
+/**
+ * 
+ * @param {Uint8Array} data 
+ * @param {*} start 
+ * @param {*} length 
+ */
+function bytesToHexString(data, start, length){
+    var str = "";
+    for(var i = start; i < start + length; i ++){
+        var num = data[i].toString(16);
+
+        while(num.length < 2){
+            num = "0" + num;
+        }
+
+        str += num + " '" + String.fromCharCode(data[i]) + "' ";
+    }
+    return str;
+}
+
+function detectFrameVersion(frameTitle){
+    // Because versions have been smashed together some how, we need a way to detect the frame's version
+    if (frameTitle[3] == "\u0000"){
+        return 2;
+    }else{
+        return 3;
+    }
 }
