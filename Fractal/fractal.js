31ad65183c7fb2890b528e1ffea38735a21c136e
DrAwesome4333 <Dr.Awesome4333@gmail.com>
2021-11-19 18:14:09 -0700

    Added support for extra data values

--- Fractal/fractal.js
+++ Fractal/fractal.js
@@ -18,177 +18,145 @@ var V_ARRAY_BUFFER = GL.createVertexArray();
 var GL_WIDTH = GL.drawingBufferWidth;
 var GL_HEIGHT = GL.drawingBufferHeight;
 
-var vertex_source = `#version 300 es
-in vec2 pos;
-in vec2 coords;
-out vec2 _pos;
-void main(){
-    gl_Position = vec4(pos.x, pos.y , 0.0, 1.0);
-    _pos = coords;
-}
-`
-var mandelbrot_frag_source = `#version 300 es
-precision highp float;
-
-const float PI = 3.1415926535897932384626433832795;
-const int COLOR_COUNT = 255;
-
-uniform sampler2D color_palette;
-in vec2 _pos;
-int iteration;
-out vec4 color;
-vec2 z;
-vec2 c;
-
-void next(inout vec2 z, in vec2 c){
-    // save old value
-    vec2 temp = z;
-    // calculate new ones
-    z.x = temp.x * temp.x - temp.y * temp.y;
-    z.y = temp.y * temp.x * 2.0;
-    z += c;
-}
-
-void main(){
-    z = vec2(0.0, 0.0);
-    c = _pos;
-    iteration = COLOR_COUNT;
-
-    for(int i = 0; i < COLOR_COUNT; i += 1){
-        next(z, c);
-        if(z.x * z.x + z.y * z.y > 4.0){
-            iteration = i;
-            break;
-        }
+var FRACTAL_INFO = {
+    'mandelbrot':{
+        'name': 'Mandelbrot',
+        'setup':`
+        _z = _data1;
+        _c = _loc;
+        `,
+        'next':`
+        // save old value
+        vec2 tmp = _z;
+        // calculate new ones
+        _z.x = tmp.x * tmp.x - tmp.y * tmp.y;
+        _z.y = tmp.y * tmp.x * 2.0;
+        _z += _c;
+        `,
+        'data1':[0,0],
+        'data2':[0,0]
+    },
+    'julia':{
+        'name': 'Julia',
+        'setup':`
+        _z = _loc;
+        _c = _data1;`,
+        'next':`
+        // save old value
+        vec2 tmp = _z;
+        // calculate new ones
+        _z.x = tmp.x * tmp.x - tmp.y * tmp.y;
+        _z.y = tmp.y * tmp.x * 2.0;
+        _z += _c;
+        `,
+        'data1':[-1.0,0],
+        'data2':[0,0]
+    },
+    'ship':{
+        'name': 'Burning Ship',
+        'setup':`
+        _z = _data1;
+        _c = _loc;`,
+        'next':`
+        // save old value
+        vec2 tmp = _z;
+        // calculate new ones
+        _z.x = tmp.x * tmp.x - tmp.y * tmp.y;
+        _z.y = 2.0 * abs(tmp.x * tmp.y);
+        _z -= _c;
+        `,
+        'data1':[0,0],
+        'data2':[0,0]
+    },
+    'juliaShip':{
+        'name': 'Burning Julia Ship',
+        'setup':`
+        _z = _loc;
+        _c = _data1;`,
+        'next':`
+        // save old value
+        vec2 tmp = _z;
+        // calculate new ones
+        _z.x = tmp.x * tmp.x - tmp.y * tmp.y;
+        _z.y = 2.0 * abs(tmp.x * tmp.y);
+        _z -= _c;
+        `,
+        'data1':[-0.598, 0.9226],
+        'data2':[0,0]
     }
-    
-    color = texture(color_palette, vec2(float(iteration) / float(COLOR_COUNT), 0.0));
-}`
-
-var julia_frag_source = `#version 300 es
-precision highp float;
-
-const float PI = 3.1415926535897932384626433832795;
-const int COLOR_COUNT = 255;
-
-uniform sampler2D color_palette;
-in vec2 _pos;
-int iteration;
-out vec4 color;
-vec2 z;
-vec2 c;
-
-void next(inout vec2 z, in vec2 c){
-    // save old value
-    vec2 temp = z;
-    // calculate new ones
-    z.x = temp.x * temp.x - temp.y * temp.y;
-    z.y = temp.y * temp.x * 2.0;
-    z += c;
 }
 
-void main(){
-    z = _pos;
-    c = vec2(-1.0, 0.0);
-    iteration = COLOR_COUNT;
-
-    for(int i = 0; i < COLOR_COUNT; i += 1){
-        next(z, c);
-        if(z.x * z.x + z.y * z.y > 4.0){
-            iteration = i;
-            break;
-        }
-    }
-    
-    color = texture(color_palette, vec2(float(iteration) / float(COLOR_COUNT), 0.0));
-}`
-
-var ship_frag_source = `#version 300 es
-precision highp float;
-
-const float PI = 3.1415926535897932384626433832795;
-const int COLOR_COUNT = 255;
-
-uniform sampler2D color_palette;
-in vec2 _pos;
-int iteration;
-out vec4 color;
-vec2 z;
-vec2 c;
-
-void next(inout vec2 z, in vec2 c){
-    // save old value
-    vec2 temp = z;
-    // calculate new ones
-    z.x = temp.x * temp.x - temp.y * temp.y;
-    z.y = 2.0 * abs(temp.x * temp.y);
-    z -= c;
-}
-
-void main(){
-    z = vec2(0.0, 0.0);
-    c = _pos;
-    iteration = COLOR_COUNT;
-
-    for(int i = 0; i < COLOR_COUNT; i += 1){
-        next(z, c);
-        if(z.x * z.x + z.y * z.y > 4.0){
-            iteration = i;
-            break;
-        }
-    }
-    
-    color = texture(color_palette, vec2(float(iteration) / float(COLOR_COUNT), 0.0));
-}`
-
-var julia_ship_frag_source = `#version 300 es
-precision highp float;
-
-const float PI = 3.1415926535897932384626433832795;
-const int COLOR_COUNT = 255;
-
-uniform sampler2D color_palette;
-in vec2 _pos;
-int iteration;
-out vec4 color;
-vec2 z;
-vec2 c;
-
-void next(inout vec2 z, in vec2 c){
-    // save old value
-    vec2 temp = z;
-    // calculate new ones
-    z.x = temp.x * temp.x - temp.y * temp.y;
-    z.y = 2.0 * abs(temp.x * temp.y);
-    z -= c;
-}
-
-void main(){
-    z = _pos;
-    c = vec2(-0.598, 0.9226);
-    iteration = COLOR_COUNT;
-
-    for(int i = 0; i < COLOR_COUNT; i += 1){
-        next(z, c);
-        if(z.x * z.x + z.y * z.y > 4.0){
-            iteration = i;
-            break;
-        }
-    }
-    
-    color = texture(color_palette, vec2(float(iteration) / float(COLOR_COUNT), 0.0));
-}`
+var fractals = {};
 /**
- * 
+ * Creates a fractal generator within a GL context.
  * @param {WebGL2RenderingContext} gl 
- * @param {WebGLProgram} shader 
+ * @param {string} setupFunc A glsl function run before each pixel calculation for the signature:
+ * void setup(inout vec2 _z, inout vec2 _c, in vec2 _loc, in vec2 _data1, in vec2 _data2)
+ * @param {string} nextFunc A glsl function to advance the fractal formula for the signature:
+ * void next(inout vec2 _z, in vec2 _c)
  */
-function Fractal(gl, shader){
+function FractalGenerator(gl, setupFunc, nextFunc){
+
+    var fragSource = `#version 300 es
+    precision highp float;
+    
+    const float PI = 3.1415926535897932384626433832795;
+    const int COLOR_COUNT = 256;
+    
+    uniform sampler2D color_palette;
+    uniform vec2 data1;
+    uniform vec2 data2;
+    uniform int maxSteps;
+    
+    in vec2 _pos;
+    out vec4 color;
+    
+    int iteration;
+    vec2 z;
+    vec2 c;
+    
+    void next(inout vec2 _z, in vec2 _c){
+    ${nextFunc}
+    }
+    
+    void setup(inout vec2 _z, inout vec2 _c, in vec2 _loc, in vec2 _data1, in vec2 _data2){
+    ${setupFunc}
+    }
+    
+    void main(){
+        setup(z, c, _pos, data1, data2);
+    
+        iteration = maxSteps;
+    
+        for(int i = 0; i < maxSteps; i += 1){
+            next(z, c);
+            if(z.x * z.x + z.y * z.y > 4.0){
+                iteration = i;
+                break;
+            }
+        }
+        
+        color = texture(color_palette, vec2(float(iteration) / float(COLOR_COUNT), 0.0));
+    }`
+
+    
+    var vertex_source = `#version 300 es
+    in vec2 pos;
+    in vec2 coords;
+    out vec2 _pos;
+    void main(){
+        gl_Position = vec4(pos.x, pos.y , 0.0, 1.0);
+        _pos = coords;
+    }`
+    var shader = buildProgram(gl, vertex_source, fragSource);
     var vArrayBuffer = gl.createVertexArray();
     gl.bindVertexArray(vArrayBuffer);
     gl.useProgram(shader);
     var posLoc = gl.getAttribLocation(shader, "pos");
     var coordLoc = gl.getAttribLocation(shader, "coords");
+    var data1 = gl.getUniformLocation(shader, "data1");
+    var data2 = gl.getUniformLocation(shader, "data2");
+    var maxSteps = gl.getUniformLocation(shader, "maxSteps");
 
     gl.bindBuffer(gl.ARRAY_BUFFER, POS_BUFFER);
     gl.bufferData(gl.ARRAY_BUFFER, POS_ARRAY, gl.STATIC_DRAW);
@@ -207,13 +175,30 @@ function Fractal(gl, shader){
 
     gl.bindVertexArray(null);
 
-    this.draw = function(AXIS_LEN, CX, CY){
+    /**
+     * Draws a the fractal with the given parameters
+     * @param {Number} axisLength The axis length
+     * @param {Number} centerX The center x component
+     * @param {Number} centerY The center y component
+     * @param {Number[]} d1 Must be a 2 number array, Input for _data1, use varies by setup and next functions defined
+     * @param {Number[]} d2 Must be a 2 number array, Input for _data2, use varies by setup and next functions defined
+     */
+    this.draw = function(axisLength, centerX, centerY, d1=[0,0], d2=[0,0]){
         gl.useProgram(shader);
         gl.bindVertexArray(vArrayBuffer);
+        gl.uniform2fv(data1, d1);
+        gl.uniform2fv(data2, d2);
+        gl.uniform1i(maxSteps, 255);
 
-        DEFAULT_COORD_ARRAY = new Float32Array([-AXIS_LEN/2+CX,AXIS_LEN/2+CY, -AXIS_LEN/2+CX,-AXIS_LEN/2+CY, AXIS_LEN/2+CX,-AXIS_LEN/2+CY, AXIS_LEN/2+CX,AXIS_LEN/2+CY]);
+        var coordArray = new Float32Array(
+            [
+                -axisLength/2+centerX, axisLength/2+centerY,
+                -axisLength/2+centerX, -axisLength/2+centerY,
+                axisLength/2+centerX, -axisLength/2+centerY,
+                axisLength/2+centerX, axisLength/2+centerY
+            ]);
         gl.bindBuffer(gl.ARRAY_BUFFER, COORD_BUFFER);
-        gl.bufferSubData(gl.ARRAY_BUFFER, 0, DEFAULT_COORD_ARRAY);
+        gl.bufferSubData(gl.ARRAY_BUFFER, 0, coordArray);
     
         gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT,0);
         gl.bindVertexArray(null);
@@ -256,11 +241,15 @@ var ELE_BUFFER = GL.createBuffer();
 var POS_ARRAY = new Float32Array([-1.0,1.0, -1.0,-1.0, 1.0,-1.0, 1.0,1.0]);
 var DEFAULT_COORD_ARRAY = new Float32Array([-1.0,1.0, -1.0,-1.0, 1.0,-1.0, 1.0,1.0]);
 
-var mandelbrot = new Fractal(GL, buildProgram(GL, vertex_source, mandelbrot_frag_source));
-var julia = new Fractal(GL, buildProgram(GL, vertex_source, julia_frag_source));
-var ship = new Fractal(GL, buildProgram(GL, vertex_source, ship_frag_source));
-var juliaShip = new Fractal(GL, buildProgram(GL, vertex_source, julia_ship_frag_source));
-var currentFractal = mandelbrot;
+for(var fName in FRACTAL_INFO){
+    let fractal = new FractalGenerator(GL, FRACTAL_INFO[fName]['setup'], FRACTAL_INFO[fName]['next']);
+    fractals[fName] = fractal;
+    var option = document.createElement('option');
+    option.value = fName;
+    option.innerText = FRACTAL_INFO[fName]['name'];
+    document.getElementById('fractal').appendChild(option);
+}
+var currentFractal = fractals['mandelbrot'];
 
 GL.activeTexture(GL.TEXTURE0)
 var COLOR_PALETTE = GL.createTexture();
@@ -311,31 +300,23 @@ GL.viewport(0, 0, GL_WIDTH, GL_HEIGHT);
 function changeFractal(){
     // @ts-ignore
     var fractalName = document.getElementById('fractal').value;
-    switch(fractalName){
-        case 'julia':
-            currentFractal = julia;
-            break;
-        case 'mandelbrot':
-            currentFractal = mandelbrot;
-            break;
-        case 'ship':
-            currentFractal = ship;
-            break;
-        case 'juliaShip':
-            currentFractal = juliaShip;
-            break;
-    }
-    drawWbgl()
+    currentFractal = fractals[fractalName];
+    dta1 = FRACTAL_INFO[fractalName]['data1'];
+    dta2 = FRACTAL_INFO[fractalName]['data2'];
+    updateGUI();
 }
 
 function drawWbgl(){
-    currentFractal.draw(axisLen, cx, cy);
+    currentFractal.draw(axisLen, cx, cy, dta1, dta2);
+    requestAnimationFrame(drawWbgl);
 }
 
 
 var axisLen = 4;
 var cx = 0;
 var cy = 0;
+var dta1 = [0,0];
+var dta2 = [0,0];
 drawWbgl();
 
 /**
@@ -343,9 +324,9 @@ drawWbgl();
  */
 function zoom(e){
     e.preventDefault();
+    updateParameters();
     axisLen += axisLen * e.deltaY/1000;
-    drawWbgl();
-    updateGUI()
+    updateGUI();
 }
 GL_CANVAS.addEventListener("wheel", zoom);
 
@@ -358,7 +339,6 @@ function updateCenter(e){
     var Y = e.clientY - GL_CANVAS.getBoundingClientRect().top - GL_HEIGHT/2;
     cx += (X/GL_WIDTH) * axisLen;
     cy += -(Y/GL_HEIGHT) * axisLen;
-    drawWbgl();
     updateGUI()
 }
 
@@ -370,6 +350,14 @@ function updateGUI(){
     document.getElementById('centerY').value = cy;
     // @ts-ignore
     document.getElementById('axisLength').value = axisLen;
+    // @ts-ignore
+    document.getElementById('data1R').value = dta1[0];
+    // @ts-ignore
+    document.getElementById('data1I').value = dta1[1];
+    // @ts-ignore
+    document.getElementById('data2R').value = dta2[0];
+    // @ts-ignore
+    document.getElementById('data2I').value = dta2[1];
 }
 
 function updateParameters(){
@@ -379,7 +367,10 @@ function updateParameters(){
     cy = Number(document.getElementById('centerY').value);
     // @ts-ignore
     axisLen = Number(document.getElementById('axisLength').value);
-    drawWbgl();
+    // @ts-ignore
+    dta1 = [Number(document.getElementById('data1R').value), Number(document.getElementById('data1I').value)];
+    // @ts-ignore
+    dta2 = [Number(document.getElementById('data2R').value), Number(document.getElementById('data2I').value)];
 }
 
 GL_CANVAS.addEventListener("click", updateCenter)
